b529be13a0be6fccc1c5276da057c7deb7b724e8
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2020-06-17 02:38:51 +0430

    Add auth module basic settings for jwt strategy.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
-@Module({})
-export class AuthModule {}
+@Module({
+    imports: [
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.register({
+            secret: 'verysecretpassword',
+            signOptions: {
+                expiresIn: '30d',
+            },
+        }),
+    ]
+})
+export class AuthModule { }
