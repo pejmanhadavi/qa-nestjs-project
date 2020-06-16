b42df382549a1e805942a62ca5fdbf0fc6b1e446
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2020-06-17 01:14:45 +0430

    Connect to sqlite.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // Connect to sqlite
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+   }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
