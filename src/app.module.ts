9a72a7147b589fb724c4229c1a138811ae0aca17
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2020-06-17 01:48:28 +0430

    Create users module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
    }),
+    UsersModule,
 
   ],
   controllers: [AppController],
