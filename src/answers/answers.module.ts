ec20c0b4b8967507cca000901f38f65e652a2ded
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2020-06-17 20:32:40 +0430

    Add create answer.

--- src/answers/answers.module.ts
+++ src/answers/answers.module.ts
@@ -4,12 +4,12 @@ import { AnswersService } from './answers.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { QuestionEntity } from 'src/questions/entities/question.entity';
-import { CategoryEntity } from 'src/categories/entities/category.entity';
 import { UserEntity } from 'src/users/entities/user.entity';
+import { AnswerEntity } from './entities/answer.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([QuestionEntity, CategoryEntity, UserEntity]),
+    TypeOrmModule.forFeature([AnswerEntity, QuestionEntity, UserEntity]),
   ],
   controllers: [AnswersController],
   providers: [AnswersService]
