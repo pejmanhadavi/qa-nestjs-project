c12ec2ca9c1e8d2ecbf7d3f7c3997c8beb7255a4
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2020-06-18 04:12:04 +0430

    Now it works.

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,17 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
 import { AuthService } from './auth.service';
 import { UsersService } from '../users/users.service';
+import { UserEntity } from '../users/entities/user.entity';
 
+const user = new UserEntity();
+user.id = 1;
+user.username = 'sample username';
+user.password = 'verysecretpassword'
 
 describe('AuthService', () => {
-  let service: AuthService;
-  let userService: UsersService;
+  let authService: AuthService;
+  let usersService: UsersService;
+  let userRepo: Repository<UserEntity>
 
   beforeEach(async () => {
-    service = new AuthService(userService);
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        UsersService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: {
+            findOne: jest.fn().mockResolvedValue(user),
+          }
+        }
+      ],
+    }).compile();
+
+    authService = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
   });
 });
