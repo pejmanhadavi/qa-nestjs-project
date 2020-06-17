9fe87a28c67dacf2ce436be806d3c021c114778d
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2020-06-17 13:28:01 +0430

    Add validate user. Add jwt extractor. Make jwt strategy better.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { sign } from 'jsonwebtoken';
+
+import { UsersService } from 'src/users/users.service';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly jwtService: JwtService,
+        private readonly usersService: UsersService,
+    ) { }
+
+    async createAccessToken(userId: number) {
+        const accessToken = sign({ userId }, 'verysecretpassword', { expiresIn: '30d' });
+        return accessToken;
+    }
+
+    async validateUser(jwtPayload: JwtPayload): Promise<any> {
+        const user = await this.usersService.findUserById(jwtPayload.id);
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+        return user;
+    }
+
+
+
+    //   ┬┬ ┬┌┬┐  ┌─┐─┐ ┬┌┬┐┬─┐┌─┐┌─┐┌┬┐┌─┐┬─┐
+    //   ││││ │   ├┤ ┌┴┬┘ │ ├┬┘├─┤│   │ │ │├┬┘
+    //  └┘└┴┘ ┴   └─┘┴ └─ ┴ ┴└─┴ ┴└─┘ ┴ └─┘┴└─
+    private jwtExtractor(request) {
+        const token = request.headers.authorization.replace('Bearer ', '').replace(' ', '');
+        return token;
+    }
+
+    //
+    // ╔╦╗╔═╗╔╦╗╦ ╦╔═╗╔╦╗╔═╗
+    // ║║║║╣  ║ ╠═╣║ ║ ║║╚═╗
+    // ╩ ╩╚═╝ ╩ ╩ ╩╚═╝═╩╝╚═╝
+    //
+    returnJwtExtractor() {
+        return this.jwtExtractor;
+    }
+
+}
---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from '../auth.service';
+import { PassportStrategy } from '@nestjs/passport';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+
+    constructor(private authService: AuthService) {
+        super({
+            jwtFromRequest: authService.returnJwtExtractor(),
+            ignoreExpiration: false,
+            secretOrKey: 'verysecretpassword',
+        });
+    }
+
+    async validate(payload: JwtPayload) {
+        const user = await this.authService.validateUser(payload);
+
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+
+        return user;
+    }
+
+}
