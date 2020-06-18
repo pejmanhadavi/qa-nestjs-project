670b3c8af4be37377e648edd266ee48d8a5a29a5
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2020-06-18 13:27:19 +0430

    Initial test for questions service.

--- src/questions/questions.service.spec.ts
+++ src/questions/questions.service.spec.ts
@@ -1,18 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { QuestionsService } from './questions.service';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+import { UserEntity } from '../users/entities/user.entity';
+import { QuestionEntity } from './entities/question.entity';
+import { CategoryEntity } from '../categories/entities/category.entity';
 
 describe('QuestionsService', () => {
-  let service: QuestionsService;
+  let questionsService: QuestionsService;
+
+  let userRepo: Repository<UserEntity>
+  let questionRepo: Repository<QuestionEntity>
+  let categoryRepo: Repository<CategoryEntity>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [QuestionsService],
+      providers: [
+        QuestionsService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: {}
+        },
+        {
+          provide: getRepositoryToken(QuestionEntity),
+          useValue: {
+          }
+        },
+        {
+          provide: getRepositoryToken(CategoryEntity),
+          useValue: {
+          }
+        },
+      ],
     }).compile();
 
-    service = module.get<QuestionsService>(QuestionsService);
+    questionsService = module.get<QuestionsService>(QuestionsService);
+    userRepo = module.get<Repository<UserEntity>>(getRepositoryToken(UserEntity));
+    questionRepo = module.get<Repository<QuestionEntity>>(getRepositoryToken(QuestionEntity));
+    categoryRepo = module.get<Repository<CategoryEntity>>(getRepositoryToken(CategoryEntity));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(questionsService).toBeDefined();
   });
 });
