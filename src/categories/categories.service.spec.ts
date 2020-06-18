9a92bef2a871a6de039acc3dee48092b00639089
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2020-06-18 11:46:13 +0430

    Add intial test for category.

--- src/categories/categories.service.spec.ts
+++ src/categories/categories.service.spec.ts
@@ -1,15 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesService } from './categories.service';
+import { Repository } from 'typeorm';
+import { CategoryEntity } from './entities/category.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('CategoriesService', () => {
   let service: CategoriesService;
+  let categoryRepo: Repository<CategoryEntity>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoriesService],
+      providers: [
+        CategoriesService,
+        {
+          provide: getRepositoryToken(CategoryEntity),
+          useValue: {
+
+          }
+        }
+      ],
     }).compile();
 
     service = module.get<CategoriesService>(CategoriesService);
+    categoryRepo = module.get<Repository<CategoryEntity>>(getRepositoryToken(CategoryEntity));
   });
 
   it('should be defined', () => {
